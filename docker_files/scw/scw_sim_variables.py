8f33e1e85b4667c6a887812b5428354439f291df
Michele Mesiti <michele.mesiti@gmail.com>
2018-12-03 17:27:00 +0000

    Other necessary changes to slurm.conf

--- docker_files/scw/scw_sim_variables.py
+++ docker_files/scw/scw_sim_variables.py
@@ -1,4 +1,4 @@
 
-slurm_sim_tools_dir=/home/slurm/slurm_sim_ws/slurm_sim_tools
-slurm_opt_dir=/home/slurm/slurm_sim_ws/slurm_opt
-slurm_sim_dir=/home/slurm/slurm_sim_ws/sim/
+slurm_sim_tools_dir="/home/slurm/slurm_sim_ws/slurm_sim_tools"
+slurm_opt_dir="/home/slurm/slurm_sim_ws/slurm_opt"
+slurm_sim_dir="/home/slurm/slurm_sim_ws/sim/"
