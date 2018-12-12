2a7ac8ba9930322c6a99cad873a27e38a2c9a6ca
Michele Mesiti <michele.mesiti@gmail.com>
2018-12-12 16:28:25 +0000

    Some fixes to scripts and comments

--- docker_files/scw/link_all.sh
+++ docker_files/scw/link_all.sh
@@ -2,16 +2,20 @@
 ORIGIN_DIR=/home/slurm/slurm_sim_ws/slurm_sim_tools/docker_files/scw
 TARGET_DIR=/install_files
 
-ln -s $ORIGIN_DIR/package_install.R $TARGET_DIR/
-ln -s $ORIGIN_DIR/startup_file.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/initial_test.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/scw_cluster_setup.py $TARGET_DIR/
-ln -s $ORIGIN_DIR/scw_ws_config.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/populate_slurmdb.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/generate_job_trace.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/run_sim.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/check_results.R $TARGET_DIR/
-ln -s $ORIGIN_DIR/download_slurm_conf_from_sunbird.sh $TARGET_DIR/
-ln -s $ORIGIN_DIR/sacctmgr_output_to_sacctmgr_commands_QOS.py $TARGET_DIR/
-ln -s $ORIGIN_DIR/scw_sim_variables.py $TARGET_DIR/
-ln -s $ORIGIN_DIR/variables.sh $TARGET_DIR/
+for file in \
+check_results.R \
+generate_job_trace.sh \
+initial_test.sh \
+package_install.R \
+populate_slurmdb.sh \
+run_sim.sh \
+sacctmgr_output_to_sacctmgr_commands_QOS.py \
+scw_cluster_setup.py \
+scw_sim_variables.py \
+scw_ws_config.sh \
+slurm_conf_setup.sh \
+startup_file.sh \
+variables.sh 
+do
+  ln -s $ORIGIN_DIR/$file $TARGET_DIR
+done
---
+++ docker_files/scw/slurm_conf_setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source /install_files/variables.sh
+
+echo "This script will download data from "'$USER_HOST(=$1)'", or untar the file"
+echo "$SCW_DATA_PACKAGE"
+echo "if no input is given."
+USER_HOST=$1
+
+
+ask_and_do(){
+
+    echo "$1"
+    echo "Execute Command? (yes || y)"
+    read answer
+    if [[ "$answer" == "yes" || "$answer" == "y" ]]
+    then
+        echo "$1" | bash
+    else
+        echo "Nothing done"
+    fi
+
+}
+
+
+
+if [ $USER_HOST != "" ]
+then
+
+  # getting historical job data from sacct
+  ask_and_do "ssh $USER_HOST 'sacct --format JobIdRaw,Submit,Timelimit,End,Start,NTasks,NNodes,User,QOS,Partition,Account,ReqMem,ReqGRES,NCPUS,State --allocations --parsable2 --allusers --starttime=092018 --endtime=102018' > $COPY_DIR_DESTINATION/sacct_output.csv "
+
+  # getting QoS information
+  ask_and_do "ssh $USER_HOST 'sacctmgr list QOS --parsable2' > $COPY_DIR_DESTINATION/QOS_data.csv"
+else 
+  if [ ! -f "$SCW_DATA_PACKAGE" ]
+  then
+    echo "Error: file $SCW_DATA_PACKAGE, containing job, qos and association data, does not exist. "
+    exit(1)
+  else
+    tar -xvf $SCW_DATA_PACKAGE
+  fi
+fi
+
+# creating user.sim file
+echo "slurm:1000" >> $SLURM_SIM_TOOLS/scw/etc/users.sim
+grep 'User' $CLUSTER_SLURMDB_DUMP | cut -d\' -f2 | sort | uniq | awk '{print $1":"1000+NR}' >> $SLURM_SIM_TOOLS/scw/etc/users.sim
+
+
+
