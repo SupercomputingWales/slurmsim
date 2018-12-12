e6508b955dc4689cc31b1384692e823b959c58cf
Michele Mesiti <michele.mesiti@gmail.com>
2018-12-12 16:04:39 +0000

    Streamlining
    Changed files in /install_files to be links pointing at files in the
    slurm_sim_tools repos - to more agility. It should be now unnecessary to
    rebuild the image.

--- docker_files/scw/variables.sh
+++ docker_files/scw/variables.sh
@@ -1,5 +1,7 @@
 
 export SLURM_SIM_TOOLS=/home/slurm/slurm_sim_ws/slurm_sim_tools
-export CLUSTER_SLURMDB_DUMP=$SLURM_SIM_TOOLS/scw/sunbird.cfg
+export COPY_DIR_DESTINATION=$SLURM_SIM_TOOLS/scw
+export CLUSTER_SLURMDB_DUMP=$COPY_DIR_DESTINATION/sunbird.cfg
 export SLURM_OPT=/home/slurm/slurm_sim_ws/slurm_opt
 export SLURM_SIM=/home/slurm/slurm_sim_ws/sim/
+export SCW_DATA_PACKAGE=$COPY_DIR_DESTINATION/scw.tar
