36fa3f61ab99a5bbc49db8b2ea48ba5ecd05b272
Michele Mesiti <michele.mesiti@gmail.com>
2019-02-06 10:53:45 +0000

    Now a slurm.conf file can be passed inside scw.tar
    and if it is found, it will be moved in the place of the original
    slurm.conf.
    Removed the option of downloading data directly from the cluster.
    Updated docs to reflect this.

--- docker_files/scw/slurm_conf_setup.sh
+++ docker_files/scw/slurm_conf_setup.sh
@@ -2,51 +2,28 @@
 
 source /install_files/variables.env
 
-echo "This script will download data from "'$USER_HOST(=$1)'", or untar the file"
+echo "This script will untar the file"
 echo "$SCW_DATA_PACKAGE"
-echo "if no input is given."
-USER_HOST=$1
 
-
-ask_and_do(){
-
-    echo "$1"
-    echo "Execute Command? (yes || y)"
-    read answer
-    if [[ "$answer" == "yes" || "$answer" == "y" ]]
-    then
-        echo "$1" | bash
-    else
-        echo "Nothing done"
-    fi
-
-}
-
-
-
-if [ "$USER_HOST" != "" ]
+if [ ! -f "$SCW_DATA_PACKAGE" ]
 then
-
-  # getting historical job data from sacct
-  ask_and_do "ssh $USER_HOST 'sacct --format JobIdRaw,Submit,Timelimit,End,Start,NTasks,NNodes,User,QOS,Partition,Account,ReqMem,ReqGRES,NCPUS,State --allocations --parsable2 --allusers --starttime=092018 --endtime=102018' > $COPY_DIR_DESTINATION/sacct_output.csv "
-
-  # getting QoS information
-  ask_and_do "ssh $USER_HOST 'sacctmgr list QOS --parsable2' > $COPY_DIR_DESTINATION/QOS_data.csv"
-else 
-  if [ ! -f "$SCW_DATA_PACKAGE" ]
+  echo "Error: file $SCW_DATA_PACKAGE, containing job, qos and association data, does not exist. "
+  exit 1
+else
+  cd $(dirname $SCW_DATA_PACKAGE)
+  tar -xvf $(basename $SCW_DATA_PACKAGE)
+  if [ -f "$NEW_SLURM_CONF" ] 
   then
-    echo "Error: file $SCW_DATA_PACKAGE, containing job, qos and association data, does not exist. "
-    exit 1
-  else
-    cd $(dirname $SCW_DATA_PACKAGE)
-    tar -xvf $(basename $SCW_DATA_PACKAGE)
-    cd -
+      echo "Found new slurm.conf. Moving it in the expected location."
+      mv $NEW_SLURM_CONF $SLURM_CONF_LOCATION
   fi
+  cd -
 fi
 
 # creating user.sim file
-echo "slurm:1000" >> $SLURM_SIM_TOOLS/scw/etc/users.sim
-grep 'User' $CLUSTER_SLURMDB_DUMP | grep -v root | cut -d\' -f2 | sort | uniq | awk '{print $1":"1000+NR}' >> $SLURM_SIM_TOOLS/scw/etc/users.sim
+USERS_SIM=$SLURM_SIM_TOOLS/scw/etc/users.sim
+echo "slurm:1000" > $USERS_SIM
+grep 'User' $CLUSTER_SLURMDB_DUMP | grep -v root | cut -d\' -f2 | sort | uniq | awk '{print $1":"1000+NR}' >> $USERS_SIM
 
 
 
