15da780b2f32bcbcb05d493e59d3f13c93005309
Michele Mesiti <michele.mesiti@gmail.com>
2019-03-14 17:54:42 +0000

    Modified simulation checks to exclude cancellation time checks if necessary

--- scw/prioritisation/simulation_checks.py
+++ scw/prioritisation/simulation_checks.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
+'''
+This script runs a number of checks on the results of the simulation.
+There are 4 files which are needed by the scipt:
+    1. jobcompo.log - containing the output of the simulation, in a format 
+       similar to sacct_output.csv.
+    2. sacct_output.csv - the corresponding live data.
+    3. test_trace.csv - the .csv file the binary version of which is fed to the
+       simulator
+    4. cancelled_jobs.csv - A list of cancelled jobs obtained from slurmctld.log,
+       in the format 'CancelTime,JobIDRaw'. Obtaining this requires grepping
+       through the slurmctld.log file, which can be hard.
+'''
+
 import pandas as pd
 import numpy as np
 from functools import reduce
 trace = pd.read_csv('test_trace.csv')
 trace = trace.set_index('sim_job_id')
 
+check_cancellation_times = False
+
 files = {
     'Sim': ('jobcomp.log', 'JobIDRaw', '|'),
     'Live': ('sacct_output.csv', 'JobIDRaw', '|'),
     'Trace': ('test_trace.csv', 'sim_job_id', ','),
-    'Canc': ('cancelled_jobs.csv', 'JobIDRaw', ',')
 }
+if check_cancellation_times: 
+    files['Canc'] = ('cancelled_jobs.csv', 'JobIDRaw', ',')
 
 
 dfs = [ (name,pd.read_csv(filename,sep=separator).set_index(idcol)) \
@@ -34,6 +50,8 @@ print(f"STATEMENT: STATUS : {result}")
 datetime_columns = {
     'End', 'Start', 'Submit', 'sim_submit', 'Eligible', 'CancelTime'
 }
+
+
 for name, df in dfs:
     for column in set(df.columns).intersection(datetime_columns):
         df[column] = pd.to_datetime(df[column])
@@ -94,58 +112,59 @@ print(
     f"INFO     : Total core seconds on the simulated system (*): {core_seconds_sim2}"
 )
 
-# CANCEL TIME CHECKS
-condition = (abs(joined.CancelTime - joined.EndLive) < threshold) | pd.isna(
-    joined.CancelTime)
-result = all(condition)
-print(
-    f"STATEMENT: For the jobs cancelled in the sim, ends differ only by {str(threshold)} max"
-)
-print(
-    f"STATEMENT: between the cancellation request to the simulator and the live data:"
-)
-print(f"STATEMENT: STATUS : {result}")
-
-threshold = np.timedelta64(5, 'm')
-job_simulation_ends_before_live = ((joined.EndSim - joined.EndLive) <
-                                   threshold)
-condition = job_simulation_ends_before_live | (~job_is_cancelled_live)
-result = all(condition)
-print(
-    f"STATEMENT: For the jobs cancelled in the sim, they end before their live"
-)
-print(
-    f"STATEMENT: counterpart or max after {str(threshold)} the live counterpart:"
-)
-print(f"STATEMENT: STATUS : {result}")
-
-threshold = np.timedelta64(5, 'm')
-short_duration_threshold = np.timedelta64(2, 's')
-job_sim_lasts_very_little = joined.durationSim < short_duration_threshold
-condition = job_sim_lasts_very_little | ~job_is_cancelled_live
-result = all(condition)
-print(
-    f"STATEMENT: Jobs that were cancelled in the live system last less than ")
-print(f"STATEMENT: {str(short_duration_threshold)} in the simulator:")
-print(f"STATEMENT: STATUS : {result}")
-
-# Check that the number of core seconds missing from the simulation is
-# explained by the cancelled jobs
-core_seconds_live_cancelled = (joined.durationLive / np.timedelta64(1, 's') *
-                               joined.NCPUSLive)[job_is_cancelled_live].sum()
-core_seconds_sim_cancelled = (joined.durationSim / np.timedelta64(1, 's') *
-                              joined.NCPUSSim)[job_is_cancelled_live].sum()
-print(
-    f"INFO     : Total core seconds on the live system in cancelled jobs: {core_seconds_live_cancelled}"
-)
-print(
-    f"INFO     : Total core seconds on the simulated system in cancelled jobs: {core_seconds_sim_cancelled}"
-)
-core_seconds_sim_cancelled2 = (joined.durationSim / np.timedelta64(1, 's') *
-                               joined.NCPUSLive)[job_is_cancelled_live].sum()
-print(
-    f"INFO     : Total core seconds on the simulated system in cancelled jobs (*): {core_seconds_sim_cancelled2}"
-)
+if check_cancellation_times:
+    # CANCEL TIME CHECKS
+    condition = (abs(joined.CancelTime - joined.EndLive) < threshold) | pd.isna(
+        joined.CancelTime)
+    result = all(condition)
+    print(
+        f"STATEMENT: For the jobs cancelled in the sim, ends differ only by {str(threshold)} max"
+    )
+    print(
+        f"STATEMENT: between the cancellation request to the simulator and the live data:"
+    )
+    print(f"STATEMENT: STATUS : {result}")
+    
+    threshold = np.timedelta64(5, 'm')
+    job_simulation_ends_before_live = ((joined.EndSim - joined.EndLive) <
+                                       threshold)
+    condition = job_simulation_ends_before_live | (~job_is_cancelled_live)
+    result = all(condition)
+    print(
+        f"STATEMENT: For the jobs cancelled in the sim, they end before their live"
+    )
+    print(
+        f"STATEMENT: counterpart or max after {str(threshold)} the live counterpart:"
+    )
+    print(f"STATEMENT: STATUS : {result}")
+    
+    threshold = np.timedelta64(5, 'm')
+    short_duration_threshold = np.timedelta64(2, 's')
+    job_sim_lasts_very_little = joined.durationSim < short_duration_threshold
+    condition = job_sim_lasts_very_little | ~job_is_cancelled_live
+    result = all(condition)
+    print(
+        f"STATEMENT: Jobs that were cancelled in the live system last less than ")
+    print(f"STATEMENT: {str(short_duration_threshold)} in the simulator:")
+    print(f"STATEMENT: STATUS : {result}")
+    
+    # Check that the number of core seconds missing from the simulation is
+    # explained by the cancelled jobs
+    core_seconds_live_cancelled = (joined.durationLive / np.timedelta64(1, 's') *
+                                   joined.NCPUSLive)[job_is_cancelled_live].sum()
+    core_seconds_sim_cancelled = (joined.durationSim / np.timedelta64(1, 's') *
+                                  joined.NCPUSSim)[job_is_cancelled_live].sum()
+    print(
+        f"INFO     : Total core seconds on the live system in cancelled jobs: {core_seconds_live_cancelled}"
+    )
+    print(
+        f"INFO     : Total core seconds on the simulated system in cancelled jobs: {core_seconds_sim_cancelled}"
+    )
+    core_seconds_sim_cancelled2 = (joined.durationSim / np.timedelta64(1, 's') *
+                                   joined.NCPUSLive)[job_is_cancelled_live].sum()
+    print(
+        f"INFO     : Total core seconds on the simulated system in cancelled jobs (*): {core_seconds_sim_cancelled2}"
+    )
 
 missing = core_seconds_live - core_seconds_sim2 - core_seconds_live_cancelled + core_seconds_sim_cancelled2
 margin = joined.NCPUSLive.sum()
@@ -155,33 +174,34 @@ print(f"STATEMENT: The number of core seconds missing from the simulation is ")
 print(f"STATEMENT: explained by the cancelled jobs:")
 print(f"STATEMENT: STATUS : {result}")
 
-# Check that cancellation time in the simulator is logged in at the right time.
-threshold = np.timedelta64(1, 's')
-condition = (abs(joined.CancelTime - joined.EndLive) < threshold) | pd.isna(
-    joined.CancelTime)
-result = all(condition)
-print(
-    f"STATEMENT: Cancellation time reported in the simulator logs coincide with"
-)
-print(
-    f"STATEMENT: the live cancellation times within {str(threshold)} (when reported):"
-)
-print(f"STATEMENT: STATUS : {result}")
-
-cancellations_logged = (~pd.isna(joined.CancelTime)).sum()
-cancellations_live = (job_is_cancelled_live).sum()
-job_is_cancelled_sim = joined.StateSim.str.contains('CANCELLED')
-cancellations_sim = (job_is_cancelled_sim).sum()
-print(
-    f"INFO     : Number of cancellation requests logged    :{cancellations_logged}"
-)
-print(
-    f"INFO     : Number of cancellation requests live      :{cancellations_live}"
-)
-print(
-    f"INFO     : Number of cancellation in simulator output:{cancellations_sim}"
-)
-
-print(f"(*): Computed using NCPUSLive instead of NCPUSSim. ")
-
-print(f"(*): Computed using NCPUSLive instead of NCPUSSim. ")
+if check_cancellation_times:
+    # Check that cancellation time in the simulator is logged in at the right time.
+    threshold = np.timedelta64(1, 's')
+    condition = (abs(joined.CancelTime - joined.EndLive) < threshold) | pd.isna(
+        joined.CancelTime)
+    result = all(condition)
+    print(
+        f"STATEMENT: Cancellation time reported in the simulator logs coincide with"
+    )
+    print(
+        f"STATEMENT: the live cancellation times within {str(threshold)} (when reported):"
+    )
+    print(f"STATEMENT: STATUS : {result}")
+    
+    cancellations_logged = (~pd.isna(joined.CancelTime)).sum()
+    cancellations_live = (job_is_cancelled_live).sum()
+    job_is_cancelled_sim = joined.StateSim.str.contains('CANCELLED')
+    cancellations_sim = (job_is_cancelled_sim).sum()
+    print(
+        f"INFO     : Number of cancellation requests logged    :{cancellations_logged}"
+    )
+    print(
+        f"INFO     : Number of cancellation requests live      :{cancellations_live}"
+    )
+    print(
+        f"INFO     : Number of cancellation in simulator output:{cancellations_sim}"
+    )
+    
+    print(f"(*): Computed using NCPUSLive instead of NCPUSSim. ")
+    
+    print(f"(*): Computed using NCPUSLive instead of NCPUSSim. ")
