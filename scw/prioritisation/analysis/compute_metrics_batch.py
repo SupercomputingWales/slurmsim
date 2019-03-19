19340718d5f7bb2c24ff9e37afe1fa2d9cdaf89d
Michele Mesiti <michele.mesiti@gmail.com>
2019-03-19 15:22:12 +0000

    Corrected bug that would forget the first line of the analysis settings
    It was seen by pandas as the name of the columns

--- scw/prioritisation/analysis/compute_metrics_batch.py
+++ scw/prioritisation/analysis/compute_metrics_batch.py
@@ -17,7 +17,11 @@ def clear_text(filename):
     with open(filename,'r') as f:
         lines = f.readlines()
         cleared_lines = [ clear_line(line) for line in lines ]
-        text = '\n'.join([ ','.join(clear_line(line).split()) for line in cleared_lines if len(line) != 1 ])
+        good_lines = [ ','.join(clear_line(line).split()) for line in cleared_lines if len(line) != 1 ]
+        print(f"[DEBUG] No of good lines: {len(good_lines)}")
+        header = ','.join(['SimulationStart','SimulationEnd',
+        'PriorityWeightQOS','AnalysisStart','AnalysisEnd'])
+        text = '\n'.join([header]+good_lines)
         while '\n\n' in text:
             text = text.replace('\n\n','\n')
         
@@ -25,9 +29,7 @@ def clear_text(filename):
 
 def read_cycle_info_file(filename):
     settings_table = pd.read_csv(StringIO(clear_text(filename)),dtype=str)
-    settings_table.columns = ['SimulationStart','SimulationEnd',
-        'PriorityWeightQOS','AnalysisStart','AnalysisEnd']
-   
+  
     return settings_table
 
 def dirname(SimulationStart,SimulationEnd,PriorityWeightQOS):
